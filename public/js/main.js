cbc12ec0049270ab00b7df02b12ab39881b577e8
Anjani <pranav.ch.12@outlook.com>
2022-04-26 16:34:44 +0200

    Added docs page

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,12 @@
 window.addEventListener('DOMContentLoaded', e => {
     // Get mobile sidenav element
-    const sidenav = document.querySelector('.sidenav');
+    const sidenavs = document.querySelectorAll('.sidenav');
+    // Get collapsible elements
+    const collapsibles = document.querySelectorAll('.collapsible');
 
     // Initialize mobile sidenav logic from materialize
-    M.Sidenav.init(sidenav);
+    M.Sidenav.init(sidenavs);
+
+    // Initialize collapsible logic from materialize
+    M.Collapsible.init(collapsibles);
 });
